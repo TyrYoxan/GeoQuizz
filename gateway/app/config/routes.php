afc44cf3056beabdc7636b9d125cefbef69695bb
tyryoxan <clem.bri25@gmail.com>
2025-02-05 17:49:45 +0100

    Update partie gateway

--- gateway/app/config/routes.php
+++ gateway/app/config/routes.php
@@ -2,6 +2,7 @@
 declare(strict_types=1);
 
 use gateway\application\actions\GenericAuthAction;
+use gateway\application\actions\GenericDirectusAction;
 use gateway\application\actions\GenericGeoquizzAction;
 use gateway\middlewares\CorsMiddleware;
 use Slim\Exception\HttpNotFoundException;
@@ -19,9 +20,14 @@ return function (\Slim\App $app): \Slim\App {
         return $response;
     });
 
+    // Service Geoquizz
     $app->get('/profil', GenericGeoquizzAction::class)->setName('profil');
     $app->get('/parties/{id}', GenericGeoquizzAction::class)->setName('getPartie');
     $app->post('/parties/create', GenericGeoquizzAction::class)->setName('createPartie');
+    $app->patch('/parties/update', GenericGeoquizzAction::class)->setName('updatePartie');
+
+    // Service Directus
+    $app->get('/directus/{id}', GenericDirectusAction::class)->setName('getImage');
 
     $app->map(['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], '/{routes:.+}', function ($request, $response) {
         throw new HttpNotFoundException($request);
