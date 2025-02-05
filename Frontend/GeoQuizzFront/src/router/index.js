593c32b17141f60d97375c31d82d5aeb2efb5e3e
felicien <felicien.dirand1@gmail.com>
2025-02-05 16:02:54 +0100

    profile stats

--- Frontend/GeoQuizzFront/src/router/index.js
+++ Frontend/GeoQuizzFront/src/router/index.js
@@ -2,6 +2,8 @@ import {createRouter, createWebHistory} from "vue-router";
 import Home from '../views/Home.vue'
 import Partie from '../views/Partie.vue'
 import Auth from '../views/Authentification.vue'
+import Profile from "../views/Profile.vue";
+import Stats from "../views/Stats.vue";
 import { inject } from 'vue'
 
 const routes = [
@@ -9,6 +11,8 @@ const routes = [
   { path: '/home', component: Home },
   { path: '/partie', component: Partie },
   { path: '/Authentification', component: Auth },
+  { path: '/profile', component: Profile },
+  { path: '/stats', component: Stats },
 ];
 
 const router = createRouter({
